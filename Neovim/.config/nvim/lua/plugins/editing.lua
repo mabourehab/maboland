0b16f1dcaf452fe92208d89abe1fd7aa9de2b0ff
Mohamed AbouRehab <mabourehabforever@gmail.com>
2025-04-11 11:39:33 +0200

    Neovim formatting and autosaving updates
    Changed auto-save plugin in Neovim to allow for better handling of
    format on auto-save.

--- Neovim/.config/nvim/lua/plugins/editing.lua
+++ Neovim/.config/nvim/lua/plugins/editing.lua
@@ -1,44 +1,55 @@
 return {
-	-- Lastplace Configuration
-	{
-		"ethanholz/nvim-lastplace",
-		config = function()
-			require("nvim-lastplace").setup()
-		end,
-	},
-	-- Autopairs Configuration
-	{
-		"echasnovski/mini.pairs",
-		version = false,
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
-	-- Surrounding Configuration
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup()
-		end,
-	},
-	-- Autosave Configuration
-	{
-		"Pocco81/auto-save.nvim",
-		config = function()
-			require("auto-save").setup()
-		end,
-	},
-	-- Indentlines Configuration
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		---@module "ibl"
-		---@type ibl.config
-		opts = {},
-		config = function()
-			require("ibl").setup()
-		end,
-	},
+  -- Lastplace Configuration
+  {
+    "ethanholz/nvim-lastplace",
+    config = function()
+      require("nvim-lastplace").setup()
+    end,
+  },
+  -- Autopairs Configuration
+  {
+    "echasnovski/mini.pairs",
+    version = false,
+    config = function()
+      require("mini.pairs").setup()
+    end,
+  },
+  -- Surrounding Configuration
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+  -- Autosave Configuration
+  -- {
+  --   "Pocco81/auto-save.nvim",
+  --   config = function()
+  --     require("auto-save").setup({
+  --       debounce_delay = 1000
+  --     })
+  --   end,
+  -- },
+  {
+    "okuuva/auto-save.nvim",
+    version = '*',
+    config = function()
+      require("auto-save").setup({
+        noautocmd = false,
+      })
+    end,
+  },
+  -- Indentlines Configuration
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function()
+      require("ibl").setup()
+    end,
+  },
 }
